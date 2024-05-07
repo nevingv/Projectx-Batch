2830c8ba6919cf872181c2f3804b7ea04f2d490f
Nevin George Varghese <nevingeorge3d@gmail.com>
2024-05-07 12:51:19 -0400

    first commit

---
+++ src/test/java/org/projectx/batch/ProjectxBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.projectx.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectxBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
